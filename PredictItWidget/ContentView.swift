ab535bf53c72c343f45ddc275734169c48f2fe58
capricorn <8250820+capricorn@users.noreply.github.com>
2024-07-04 19:07:24 -0600

    Initial Commit

---
+++ PredictItWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PredictItWidget
+//
+//  Created by Collin Palmer on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PredictItWidget/PredictItWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictItWidgetApp.swift
+//  PredictItWidget
+//
+//  Created by Collin Palmer on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PredictItWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
