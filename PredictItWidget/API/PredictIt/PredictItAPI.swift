9836c6c1db0624284ea0717e0c71652ea7c9f07b
capricorn <8250820+capricorn@users.noreply.github.com>
2024-07-04 21:57:41 -0600

    Force XML response from API (until json impl)

--- PredictItWidget/API/PredictIt/PredictItAPI.swift
+++ PredictItWidget/API/PredictIt/PredictItAPI.swift
@@ -12,8 +12,10 @@ enum PredictItAPI {
     static let apiBasePath = URL(string: "https://www.predictit.org/api")!
 
     static func fetchMarketData(marketId: String) async throws -> PIMarketData {
-        let req = URLRequest(url: apiBasePath.appending(path: "marketdata/markets/\(marketId)"))
-        let (data, resp) = try await URLSession.shared.data(for: req)
+        var req = URLRequest(url: apiBasePath.appending(path: "/marketdata/markets/\(marketId)"))
+        // TODO: Switch over to json (May as well keep the xml encoding version)
+        req.setValue("application/xml", forHTTPHeaderField: "Accept")
+        let (data, _) = try await URLSession.shared.data(for: req)
         
         // TODO: Handle status codes? (Anything non-200)
         // TODO: Handle API errors..?
