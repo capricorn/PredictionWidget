edb039c7a6d9ec1134c8d7d977fc6e176b6b3c4f
capricorn <8250820+capricorn@users.noreply.github.com>
2024-07-06 20:55:47 -0600

    Create skeleton for widget

---
+++ PredictionMarketWidget/PredictionMarketWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PredictionMarketWidgetBundle.swift
+//  PredictionMarketWidget
+//
+//  Created by Collin Palmer on 7/6/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PredictionMarketWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PredictionMarketWidget()
+    }
+}
