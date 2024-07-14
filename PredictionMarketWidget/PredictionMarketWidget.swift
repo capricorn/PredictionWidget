2824b486c13c5b124432cfc1b560b761150af6ff
capricorn <8250820+capricorn@users.noreply.github.com>
2024-07-14 15:15:48 -0600

    Move timeline provider to separate file

--- PredictionMarketWidget/PredictionMarketWidget.swift
+++ PredictionMarketWidget/PredictionMarketWidget.swift
@@ -9,48 +9,6 @@ import WidgetKit
 import SwiftUI
 import SwiftData
 
-private extension PIJSONMarketContract {
-    var contract: MarketContract {
-        MarketContract(id: self.id, name: self.shortName, cents: self.lastTradePrice, change: nil)
-    }
-}
-
-struct Provider: AppIntentTimelineProvider {
-    let modelContext: ModelContext
-    
-    func placeholder(in context: Context) -> MarketEntry {
-        MarketEntry(date: Date.now, type: .market(Market(id: 0, name: "Test Market", contracts: [])))
-    }
-
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> MarketEntry {
-        MarketEntry(date: Date.now, type: .market(Market(id: 0, name: "Test Market", contracts: [])))
-    }
-    
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<MarketEntry> {
-        let defaults = UserDefaults.predictionWidget
-        var entries: [MarketEntry] = []
-        
-        do {
-            if let marketId = configuration.selectedMarket?.id {
-                let marketData = try await PredictItAPI.fetchMarketData(marketId: "\(marketId)")
-                let entry = MarketEntry(
-                    date: Date.now,
-                    type: .market(Market(
-                        id: marketData.id,
-                        name: marketData.shortName,
-                        contracts: marketData.contracts.map({$0.contract}))))
-                entries.append(entry)
-            } else {
-                entries.append(MarketEntry(date: .now, type: .market(nil)))
-            }
-        } catch {
-            entries.append(MarketEntry(date: .now, type: .error))
-        }
-        
-        return Timeline(entries: entries, policy: .after(Date.now.addingTimeInterval(60*15)))
-    }
-}
-
 struct PredictionMarketWidget: Widget {
     let kind: String = "PredictionMarketWidget"
     let context = ModelContext(try! ModelContainer(for: PreviousMarketDataModel.self, ContractEntryModel.self, MarketEntryModel.self, configurations: ModelConfiguration()))
